b7950c33352bb0ae04620ff36f22c6d4a8a0e69e
Vinod Jayasinghe <jayasinghe.mail.me@gmail.com>
2026-08-24 07:42:16 +1000

    add Angular authentication flow

--- frontend/todo-web/src/app/app.routes.ts
+++ frontend/todo-web/src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {
+  authGuard,
+  guestGuard,
+} from './core/auth/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    canActivate: [
+      guestGuard,
+    ],
+    loadComponent: () =>
+      import(
+        './features/auth/login/login'
+      ).then(
+        (module) => module.Login,
+      ),
+    title: 'Sign in | Todo App',
+  },
+
+  {
+    path: 'todos',
+    canActivate: [
+      authGuard,
+    ],
+    loadComponent: () =>
+      import(
+        './features/todos/todos'
+      ).then(
+        (module) => module.Todos,
+      ),
+    title: 'My Todos | Todo App',
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'todos',
+  },
+
+  {
+    path: '**',
+    redirectTo: 'todos',
+  },
+];
